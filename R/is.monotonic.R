08b153d74ef4beb5c113d455c1b1fb8b3eabaa0e
Davide Massidda <davide.massidda@gmail.com>
2017-04-28 11:45:52 +0200

    added na.rm option

--- R/is.monotonic.R
+++ R/is.monotonic.R
@@ -1,5 +1,7 @@
-is.monotonic <- function(x, decreasing=FALSE)
+is.monotonic <- function(x, decreasing=FALSE, na.rm=TRUE)
 {
+    if(na.rm)
+        x <- x[!is.na(x)]
     d <- x[-length(x)] - x[-1]
     if(!decreasing)
         check <- all(d <= 0)
