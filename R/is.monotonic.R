2663495f85080e1858a8bce702901df4b714ff74
Davide Massidda <davide.massidda@gmail.com>
2017-08-01 22:37:31 +0200

    Update is.monotonic.R

--- R/is.monotonic.R
+++ R/is.monotonic.R
@@ -1,11 +1,24 @@
-is.monotonic <- function(x, decreasing=FALSE, na.rm=TRUE)
+is.monotonic <- function(x, direction=c("both","forward","backward"), decreasing=NULL, na.rm=TRUE)
 {
+    # Warning: the argument decreasing is deprecated.
+    # It's kept only for backward compatibility.
+    direction <- direction[1]
+    direction <- match.arg(direction)
+    if(!is.null(decreasing)) {
+        if(!decreasing & direction=="both")
+            direction <- "forward"
+        else
+            if(decreasing & direction=="both")
+            direction <- "backward"
+    }
     if(na.rm)
         x <- x[!is.na(x)]
-    d <- x[-length(x)] - x[-1]
-    if(!decreasing)
-        check <- all(d <= 0)
+    n <- length(x)
+    d <- x[-n] - x[-1]
+    check <- c(all(d <= 0), all(d >= 0))
+    if(direction=="both")
+        check <- check[1] | check[2]
     else
-        check <- all(d >= 0)
+        check <- check[(direction=="backward")+1]
     return(check)
 }
